c4f16fe2474910f94ce74483898ed580aa54c751
sejal99 <sejal8974@gmail.com>
2024-01-08 20:35:08 +0530

    cookies implemented

--- server/middlewares/authentication.ts
+++ server/middlewares/authentication.ts
@@ -1,39 +1,39 @@
-import { Request, Response, NextFunction } from 'express'
-import jwt, { VerifyErrors } from 'jsonwebtoken'
+// import { Request, Response, NextFunction } from 'express'
+// import jwt, { VerifyErrors } from 'jsonwebtoken'
 
-export const verifyJwt= async(req:Request ,res:Response, next:NextFunction)=> {
-    try{
-        const token = req.cookies['token']
-        console.log('sssssssssssssss',token);
+// export const verifyJwt= async(req:Request ,res:Response, next:NextFunction)=> {
+//     try{
+//         const token = req.cookies['token']
+//         console.log('sssssssssssssss',token);
         
-        if(!token){
-            return res.send('Cookie expired / no cookie')
-        }
+//         if(!token){
+//             return res.send('Cookie expired / no cookie')
+//         }
  
         
-        if(!process.env.SECRET){
-            return res.sendStatus(403)
-        }
+//         if(!process.env.SECRET){
+//             return res.sendStatus(403)
+//         }
 
-        jwt.verify(token, process.env.SECRET, (err:VerifyErrors|null , payload:any)=> {
-            if(err){
-                return res.status(400).json(err)
-            }
-            if(!payload){
-                return res.sendStatus(403)
-            }
+//         jwt.verify(token, process.env.SECRET, (err:VerifyErrors|null , payload:any)=> {
+//             if(err){
+//                 return res.status(400).json(err)
+//             }
+//             if(!payload){
+//                 return res.sendStatus(403)
+//             }
 
-            if(typeof payload === "string"){
-                return res.sendStatus(403)
-            }
+//             if(typeof payload === "string"){
+//                 return res.sendStatus(403)
+//             }
 
-            req.headers["userId"]= payload.id
-            req.headers["role"]=payload.role
-            next()
+//             req.headers["userId"]= payload.id
+//             req.headers["role"]=payload.role
+//             next()
 
-         })
+//          })
         
-    }catch(err){
-        res.status(403).json(err)
-    }
-}
+//     }catch(err){
+//         res.status(403).json(err)
+//     }
+// }
